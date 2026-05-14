f36ef148e91373102fc22b81520a63a36320638c
Raju Kola <rajukola754@gmail.com>
2026-05-14 22:38:46 +0200

    feat(pdf): implement PDF generation APIs for CV, cover letter, and recruiter message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api import job, cv, message
 from api.cover_letter import router as cover_letter_router
+from api.pdf import router as pdf_router
 
 
 app = FastAPI()
@@ -9,3 +10,4 @@ app.include_router(job.router)
 app.include_router(cv.router)
 app.include_router(message.router)
 app.include_router(cover_letter_router)
+app.include_router(pdf_router)
